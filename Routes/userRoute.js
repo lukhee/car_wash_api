d0291031a34e697b38dfc403e9af30cd31f24025
balogun lukman <o.balogun@ymail.com>
2020-05-02 13:36:46 +0100

    user route change

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -14,6 +14,8 @@ const { check } = require('express-validator')
 router.post('/',  [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter password with 6 or more character').isLength({ min: 6 })], res.send("hello user") )
+    check('password', 'Please enter password with 6 or more character').isLength({ min: 6 })], (req, res)=>{
+        res.send("hello user")
+    } )
 
 module.exports = router
